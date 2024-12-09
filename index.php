64d96fc307483df7acddf57a829a976da83d5a18
Dhanashree-02 <dhanashree.sonune02@gmail.com>
2024-12-09 13:03:02 -1000

    Admin

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
                             class="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex"
                             data-bs-toggle="modal"
                             data-bs-target="#searchModal"><i
-                                class="fas fa-search"></i></button>
+                                class="fas fa-searcgith"></i></button>
                         <a href="userLogin.php"
                             class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Order
                             Now</a>
